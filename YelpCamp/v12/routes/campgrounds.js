9d3f421328fec9d6951b2352d457cc8d4387e35f
Nasser X <nao1422@hotmail.com>
2020-02-29 18:20:39 +0300

    Done.

--- YelpCamp/v12/routes/campgrounds.js
+++ YelpCamp/v12/routes/campgrounds.js
@@ -22,6 +22,7 @@ router.post("/", middleware.isLoggedIn, function (req, res) {
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
+    var price = req.body.price;
     var author = {
         id: req.user._id,
         username: req.user.username
@@ -30,7 +31,8 @@ router.post("/", middleware.isLoggedIn, function (req, res) {
         name: name,
         image: image,
         description: description,
-        author: author
+        author: author,
+        price: price
     }
     Campground.create(campground, function (err, Created) {
         if (err) {
